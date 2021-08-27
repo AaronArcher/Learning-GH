7b243bd4f8246d2ecfa9c15edc365fb5b19709cd
Aaron Johncock <aaron.johncock@hotmail.co.uk>
2021-08-27 17:47:20 +0100

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Aaron Johncock on 27/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
